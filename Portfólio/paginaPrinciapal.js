c08309470fbbaf93a0ad4c2f219ca50b0086a171
Rafael Martins <rafael2007martins16@gmail.com>
2025-11-15 18:41:45 -0300

    Update paginaPrinciapal.js

--- Portfólio/paginaPrinciapal.js
+++ Portfólio/paginaPrinciapal.js
@@ -1,10 +1,1 @@
-function goEnsinoMedio() {
-window.location.href = "EnsinoMedio/EnsinoMedio.html"
-}   
-function goTecnico() {
-    window.location.href = "Tecnico/Tecnico.html"
-}   
-function goPPE() {
-    window.location.href = "EnsinoMedio/EnsinoMedio.html"
-}   
-        
+
